bfe6eeb59b6703b3b33b8f7d5698fbde2872d769
Ariel Adams <arieladams@PearyTheMactypus.local>
2022-09-17 13:45:53 -0600

    init

---
+++ src/site-meta.config.ts
@@ -0,0 +1,11 @@
+const siteMeta = {
+  title: "Astro Book Blog",
+  description: "A blog for my bookish thoughts & features",
+  lang: "en-GB",
+  ogLocale: "en_GB",
+  githubUrl: "https://github.com/arielbot/astro-book-blog",
+  themeColorLight: "#fafafa",
+  themeColorDark: "#1d1f21",
+}
+
+export default siteMeta;
