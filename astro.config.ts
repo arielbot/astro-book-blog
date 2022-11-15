ad72eeb86f02a7b0785ca25481c1c581af4da34f
Ariel Adams <arieladams@PearyTheMactypus.local>
2022-11-14 22:37:28 -0700

    netlify url update

--- astro.config.ts
+++ astro.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
 			wrap: true,
 		},
 	},
-	site: "https://effortless-trifle-ff0560.netlify.app/",
+	site: "https://getbookishprompts.netlify.app/",
 	integrations: [
 		mdx({}),
 		tailwind({
